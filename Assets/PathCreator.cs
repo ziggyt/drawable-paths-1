c68f485dbc50cf11a4a2bd6d4891a2b2c4965462
ziggy <frmax@student.chalmers.se>
2020-03-07 23:21:57 +0100

    Initial commit

---
+++ Assets/PathCreator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+
+    private List<Vector3> _points = new List<Vector3>();
+
+    public Action<IEnumerable<Vector3>> OnNewPathCreated = delegate { };
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Cleared Points");
+            _points.Clear();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (DistanceToLastPoint(hitInfo.point) > 1f)
+                {
+                    Debug.Log("Added Point");
+                    _points.Add(hitInfo.point);
+
+                    _lineRenderer.positionCount = _points.Count;
+                    _lineRenderer.SetPositions(_points.ToArray());
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Called OnNewPathCreated, passed " + _points.Count.ToString());
+            OnNewPathCreated(_points);
+        }
+    }
+
+    private float DistanceToLastPoint(Vector3 point)
+    {
+        return !_points.Any() ? Mathf.Infinity : Vector3.Distance(_points.Last(), point);
+    }
+}
---
+++ Assets/PathMover.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathMover : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    private Queue<Vector3> _pathPoints = new Queue<Vector3>();
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        FindObjectOfType<PathCreator>().OnNewPathCreated += SetPoints;
+    }
+
+    private void SetPoints(IEnumerable<Vector3> points)
+    {
+        Debug.Log(points.Count());
+        _pathPoints = new Queue<Vector3>();
+        LerpVector3IEnumerable(points);
+    }
+
+
+    private void LerpVector3IEnumerable(IEnumerable<Vector3> points)
+    {
+        float totalDistance = Vector3.Distance(points.First(), points.Last());
+
+        for (int i = 0; i < points.Count() - 1; i++)
+        {
+            var tmpDist = Vector3.Distance(points.ElementAt(i), points.ElementAt(i + 1));
+
+            float fractionOfJourney = tmpDist / totalDistance;
+            _pathPoints.Enqueue(Vector3.Lerp(points.ElementAt(i), points.ElementAt(i + 1), fractionOfJourney));
+        }
+
+        _pathPoints.Enqueue(points.Last());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePathing();
+    }
+
+    private void UpdatePathing()
+    {
+        //Debug.Log(ShouldSetDestination());
+        if (ShouldSetDestination())
+        {
+            _navMeshAgent.SetDestination(_pathPoints.Dequeue());
+        }
+    }
+
+    private bool ShouldSetDestination()
+    {
+        //Debug.Log(_pathPoints.Count);
+
+        if (_pathPoints.Count == 0)
+        {
+            return false;
+        }
+
+        return _navMeshAgent.hasPath == false || _navMeshAgent.remainingDistance < 0.5f;
+    }
+}
